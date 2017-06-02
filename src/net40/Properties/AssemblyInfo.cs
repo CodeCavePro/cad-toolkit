f7c92bba55f5d3f7c54c1a2664747b4881c398c4
salaros <salaros@yandex.ru>
2017-06-02 15:43:27 +0300

    splitting CAD.Toolkit project into two NuGet packages
    targeting .NET 4.0 and .NET Standard 2.0

---
+++ src/net40/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CodeCave.CAD.Toolkit")]
+[assembly: AssemblyDescription("A toolkit for processing .dwg, .dxf etc without AutoCAD or RealDWG")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CodeCave, PUE")]
+[assembly: AssemblyProduct("CAD.Toolkit")]
+[assembly: AssemblyCopyright("Copyright 2016 (c) CodeCave, PUE")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a11b82e2-ea76-428d-ad5e-d9c7a501c1c4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.0.*")]
+[assembly: NeutralResourcesLanguage("en")]
+
